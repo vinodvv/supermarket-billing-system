c81cec84b751dc18f53e081ac554765439cfb68c
vinodvv <vinovijayan@gmail.com>
2024-12-11 22:35:48 +0530

    Refactor supermarket_billing.py, add rupee symbol

--- supermarket_billing.py
+++ supermarket_billing.py
@@ -1,46 +1,54 @@
-print("Welcome to the Supermarket Billing System!")
+RUPEE_SYMBOL = "\u20B9"
 
-# Get the number of items
-num_items = int(input("Enter the number of items: "))
+def supermarket_billing():
+    print("Welcome to the Supermarket Billing System!")
 
-items = []
+    # Get the number of items
+    num_items = int(input("Enter the number of items: "))
 
-for i in range(1, num_items + 1):
-    print(f"\nItem {i}:")
-    name = input("Name: ")
-    price = float(input("Price per unit: "))
-    qty = float(input("Quantity: "))
+    items = []
 
-    items.append({
-        "name": name,
-        "price": price,
-        "qty": qty
-    })
+    for i in range(1, num_items + 1):
+        print(f"\nItem {i}:")
+        name = input("Name: ")
+        price = float(input(f"Price per unit: {RUPEE_SYMBOL} "))
+        qty = float(input("Quantity: "))
 
-# Bill summary calculations
-print("\n--- Bill Summary ---")
+        items.append({
+            "name": name,
+            "price": price,
+            "qty": qty
+        })
 
-sub_total = 0
+    # Bill summary calculations
+    print("\n--- Bill Summary ---")
 
-for item in items:
-    item_total = item['price'] * item['qty']
-    sub_total += item_total
-    print(f"{item['name']}: {item['price']} x {item['qty']} = {item_total}")
+    sub_total = 0
 
-# Apply discount if any (e.g., discount for subtotal above 50.0
-discount = 0
-if sub_total >= 50:
-    discount = 0.1 * sub_total  # 10% discount
+    for item in items:
+        item_total = item['price'] * item['qty']
+        sub_total += item_total
+        print(f"{item['name']}: {RUPEE_SYMBOL}{item['price']} x {item['qty']} = {RUPEE_SYMBOL}{item_total}")
 
-# Calculate Sales Tax
-tax_rate = 0.05  # 5%
-tax = (sub_total - discount) * tax_rate
+    # Apply discount if any (e.g., discount for subtotal above 50.0
+    discount = 0
+    if sub_total >= 50:
+        discount = 0.1 * sub_total  # 10% discount
 
-# Calculate Total
-total = sub_total - discount + tax
+    # Calculate Sales Tax
+    tax_rate = 0.05  # 5%
+    tax = (sub_total - discount) * tax_rate
 
-print(f"\nSub-Total: {sub_total:.2f}")
-print(f"Discount: {discount:.2f}")
-print(f"Sales Tax: {tax:.2f}")
-print(f"Total: {total:.2f}")
-print(f"\nThank you for shopping with us!")
+    # Calculate Total
+    total = sub_total - discount + tax
+
+    print(f"\nSub-Total: {RUPEE_SYMBOL}{sub_total:.2f}")
+    print(f"Discount: {RUPEE_SYMBOL}{discount:.2f}")
+    print(f"Sales Tax: {RUPEE_SYMBOL}{tax:.2f}")
+    print(f"Total: {RUPEE_SYMBOL}{total:.2f}")
+    print(f"\nThank you for shopping with us!")
+
+
+# Run the programme
+if __name__ == "__main__":
+    supermarket_billing()
